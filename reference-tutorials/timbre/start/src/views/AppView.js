d03d28c5b6e5036bbbb2da338a924e9eb103c43c
Bryan L. Venable <bryanlvenable@gmail.com>
2015-02-23 15:12:23 -0800

    menu toggle options

--- reference-tutorials/timbre/start/src/views/AppView.js
+++ reference-tutorials/timbre/start/src/views/AppView.js
@@ -5,11 +5,14 @@ define(function(require, exports, module) {
     var Surface       = require('famous/core/Surface');
     var Transform     = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
+    var Easing = require('famous/transitions/Easing');
 
     var PageView = require('views/PageView');
 
     function AppView() {
         View.apply(this, arguments);
+
+        this.menuToggle = false;
 
         _createPageView.call(this);
         _setListeners.call(this);
@@ -17,8 +20,31 @@ define(function(require, exports, module) {
 
     AppView.prototype = Object.create(View.prototype);
     AppView.prototype.constructor = AppView;
+    
+    AppView.prototype.toggleMenu = function() {
+        if(this.menuToggle) {
+            this.slideLeft();
+        } else {
+            this.slideRight();
+        }
+        this.menuToggle = !this.menuToggle;
+    };
 
-    AppView.DEFAULT_OPTIONS = {};
+    AppView.prototype.slideRight = function() {
+        this.pageModifier.setTransform(Transform.translate(this.options.openPosition, 0, 0), this.options.transition);
+    };
+
+    AppView.prototype.slideLeft = function() {
+        this.pageModifier.setTransform(Transform.translate(0, 0, 0), this.options.transition);
+    };
+
+    AppView.DEFAULT_OPTIONS = {
+        openPosition: 276,
+        transition: {
+            duration: 300,
+            curve: Easing.inOutBack
+        }
+    };
 
     function _createPageView() {
         this.pageView = new PageView();
@@ -28,9 +54,10 @@ define(function(require, exports, module) {
     }
 
     function _setListeners() {
-        this.pageView.on('menuToggle', function(){
-            console.log('menu toggle!');
-        }.bind(this));
+        // this.pageView.on('menuToggle', function(){
+        //     console.log('menu toggle!');
+        // }.bind(this));
+        this.pageView.on('menuToggle', this.toggleMenu.bind(this));
     }
 
     module.exports = AppView;

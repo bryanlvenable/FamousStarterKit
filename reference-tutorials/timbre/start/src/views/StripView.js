03077c7bb3de429e86965e44cca26ac7ebf3581c
Bryan L. Venable <bryanlvenable@gmail.com>
2015-02-23 15:54:52 -0800

    rotated and skewed strip

--- reference-tutorials/timbre/start/src/views/StripView.js
+++ reference-tutorials/timbre/start/src/views/StripView.js
@@ -17,18 +17,31 @@ define(function(require, exports, module) {
 
     StripView.DEFAULT_OPTIONS = {
         width: 320,
-        height: 55
+        height: 55,
+        angle: -0.2
     };
 
     function _createBackground() {
         var backgroundSurface = new Surface({
             size: [this.options.width, this.options.height],
             properties: {
-                backgroundColor: 'black'
+                backgroundColor: 'black',
+                // on certain devices, a skewed surface can have jagged edges
+                // the 1px box-shadow profides some anti-aliasing to soften this
+                boxShadow: '0 0 1px rbga(0,0,0,1)'
             }
         });
 
-        this.add(backgroundSurface);
+        var rotateModifier = new StateModifier({
+            transform: Transform.rotateZ(this.options.angle)
+        });
+
+        var skewModifier = new StateModifier({
+            transform: Transform.skew(0, 0, this.options.angle)
+        });
+
+        // We are first skewing our surface then rotating it
+        this.add(rotateModifier).add(skewModifier).add(backgroundSurface);
     }
 
     module.exports = StripView;
